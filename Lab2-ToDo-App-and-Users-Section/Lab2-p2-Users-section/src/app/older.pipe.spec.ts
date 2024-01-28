7656530f38c4f3125207a9607f5e3121ec10939a
Hussein Elmlah <147069168+hussein-elmlah@users.noreply.github.com>
2024-01-28 11:44:58 +0200

    Delete Lab2-ToDo-App-and-Users-Section/Lab2-p2-Users-section directory

--- Lab2-ToDo-App-and-Users-Section/Lab2-p2-Users-section/src/app/older.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OlderPipe } from './older.pipe';
-
-describe('OlderPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OlderPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
